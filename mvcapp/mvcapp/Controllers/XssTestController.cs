0e5b0517f73a648021f5f0a2c9c946ec33e0cbea
gggttt <grzegorz.trawinsky@gmail.com>
2017-09-24 09:38:51 +0200

    web api xss test

---
+++ mvcapp/mvcapp/Controllers/XssTestController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using mvcapp.Models;
+
+namespace mvcapp.Controllers
+{
+    public class XssTestController : ApiController
+    {
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/xsstest/get")]
+        public JsonResult Get(string value)
+        {
+            return new JsonResult {Data = value};
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/xsstest/post")]
+        public JsonResult Post(XssTestModel model)
+        {
+            return new JsonResult { Data = model.Value };
+        }
+    }
+}
